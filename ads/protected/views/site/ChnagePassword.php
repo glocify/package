1d58cd5d3afa7b6e8d79b2e3c61225e63a529173
glocify <ganeshcro@gmail.com>
2017-11-03 17:17:57 +0530

    Files

--- ads/protected/views/site/ChnagePassword.php
+++ ads/protected/views/site/ChnagePassword.php
@@ -4,16 +4,16 @@ $base_url = Yii::app()->getBaseUrl(true);
 
 <div class="login">
  <div class="container">
-    <div class="row">
+    <div class="row" style="min-height:420px;">
 	<div>
 		<?php if(Yii::app()->user->hasFlash('error')): ?>
-			<div class="flash-error">
-				<font color="#d30000;"><?php echo Yii::app()->user->getFlash('error'); ?></font>
+			<div class="flash-error alert alert-danger" style="margin-top:30px;">
+				<?php echo Yii::app()->user->getFlash('error'); ?>
 			</div>
 		<?php endif; ?>
 	</div>
 	<div class="col-xs-12 col-sm-6 new-user">
-		<h2>Chnage Your Password:</h2>
+		<h2>Change Your Password:</h2>
 		<ol style="padding: 10px; margin-left: 10px;">
 			<li>New and Old Password should not be empty.</li>
 			<li>Enter Your Correct Old Password.</li>
@@ -26,11 +26,11 @@ $base_url = Yii::app()->getBaseUrl(true);
 			<div id="forgetresponse" style="display:none;"></div>
 			<div class="form-group">
 			<label for="exampleInputEmail1">Old Password:<sup>*</sup></label>
-			<input type="text" name="oldpassword" required class="form-control" id="femailid" placeholder="Old Password" value="<?php echo @$_POST['oldpassword'];?>" />
+			<input type="password" name="oldpassword" required class="form-control" id="femailid" placeholder="Old Password" value="<?php echo @$_POST['oldpassword'];?>" />
 			</div>
 			<div class="form-group">
 			<label for="exampleInputEmail1">New Password:<sup>*</sup></label>
-			<input type="text" name="newpassword" required class="form-control" id="femailid" placeholder="New Password" value="<?php echo @$_POST['newpassword'];?>" />
+			<input type="password" name="newpassword" required class="form-control" id="femailid" placeholder="New Password" value="<?php echo @$_POST['newpassword'];?>" />
 			</div>
 			<button type="submit" class="btn btn-default" name="Changepassbut" id="changepassword">Change Password</button>
 		</form>	
